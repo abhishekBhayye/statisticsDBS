256594ea93f3d775fec2d3a1cf8d21cd41f23c1b
abhishekBhayye <bhayyeabhishek@gmail.com>
2019-10-19 18:23:16 +0100

    Intial commit for project

--- server.R
+++ server.R
@@ -1,1 +1,17 @@
+library(shiny)
 
+function(input, output){
+  
+  output$histogram <- renderPlot({
+    
+    if(input$models == "bino"){
+      par(mfrow=c(2,1))
+      d <- density(rbinom(1000,input$n,input$p))
+      plot(d, main = "Binomial Distribution of generaed Data")
+      polygon(d, col = "yellow", border = "red")
+      x=0:input$n
+      plot(x,dbinom(x,input$n,input$p))
+    }
+    
+  })
+}
--- ui.R
+++ ui.R
@@ -1,1 +1,36 @@
+library(shiny)
+library(shinydashboard)
 
+header <- dashboardHeader(
+  title = "Interactive Plots"
+)
+
+body <- dashboardBody(
+  fluidPage(
+    column(width = 4,
+           selectInput("models", "SELECT MODEL:",
+                       c(
+                         "BERNOULLI"= "bern",
+                         "BINOMIAL" = "bino"
+                       )
+           ),
+           
+           sliderInput("n","parameter n in Binomial", min = 1, max = 1000, value = 10),
+           
+           conditionalPanel(
+             "input.models == 'bino'",
+             numericInput("p","parameter p in Binomial", min = 0, max = 1, value = "0.5")
+           )
+    ),
+    
+    column(width = 8,
+           plotOutput("histogram")
+    )
+  )   
+)
+
+dashboardPage(
+  header,
+  dashboardSidebar(disable = TRUE),
+  body
+)
