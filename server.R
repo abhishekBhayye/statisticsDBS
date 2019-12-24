75581074290fbb5e9ee9ab9ef0f56e80d4b72518
abhishekBhayye <bhayyeabhishek@gmail.com>
2019-12-24 22:36:43 +0000

    Merged Models, hypothesis, glm in one file.

--- server.R
+++ server.R
@@ -1,6 +1,9 @@
 library(shiny)
 
-function(input, output){
+function(input, output, session){
+  
+  ###>>> Begin >>> Abhishek Bhayye (10527126) >>> 
+  
   dataForInput <- reactiveVal(NULL)
   
   myData <- reactive({
@@ -37,6 +40,333 @@ function(input, output){
       "No. of Columns", paste0(ncol(myData())), icon = icon("cog", lib = "glyphicon"),
       color = "yellow", fill = TRUE
     )
+  })
+  
+  
+  ###>>> Begin >>> Ashwini Kadam(Student#10527552) >>> 
+  
+  output$histogram <- renderPlot({ 
+    
+    # binomial  
+    if (input$dismodel == 'binomial') { 
+      par(mfrow=c(1,2))  
+      d <- density(rbinom(1000,input$n,input$p))  
+      plot(d, main="Kernel Density of generated data")  
+      polygon(d, col="red", border="blue") 
+      x=0:input$n  
+      plot(x,dbinom(x,input$n,input$p))  
+    } 
+    
+    # poisson 
+    if (input$dismodel == 'poisson') { 
+      par(mfrow=c(1,2))   
+      D=rpois(input$s, input$lam)  
+      tab=table(D)  
+      barplot(tab,col='blue')  
+      x1=0:input$max  
+      y1=dpois(x1,input$lam)  
+      plot(x1,y1,type='b')  
+    } 
+    
+    # geometric  
+    if (input$dismodel == 'geometric') { 
+      par(mfrow=c(1,2)) 
+      D=rgeom(input$s, input$p)  
+      tab=table(D)  
+      barplot(tab,col='blue')  
+      x2=0:input$max  
+      y2=dgeom(x2,input$p)  
+      plot(x2,y2,type='b')  
+    } 
+    
+    # bernoulli      
+    if (input$dismodel == 'bernoulli') { 
+      par(mfrow=c(1,2))
+      Density <- density(rbinom(input$s,1,input$p))
+      plot(Density, main="Kernel Density of generated data")
+      polygon(Density, col="red", border="blue")
+      x=0:1
+      plot(x,dbinom(x,1,input$p))
+    }
+    
+    # hypergeometric
+    if (input$dismodel == 'hypergeometric') { 
+      par(mfrow=c(1,2))
+      D=rhyper(nn=input$s, m=input$m, n=input$n, k=rep(input$k, input$s))
+      tab=table(D)
+      barplot(tab,col='blue')
+      x2=0:input$s
+      y2=dhyper(x2, m=input$m, n=input$n, k=input$k, log=FALSE)
+      plot(x2,y2,type='b')
+    }
+  })
+  
+  ###>>> Begin >>> Himaja Mineni (10529371) >>> 
+  
+  observeEvent(input$do, {   
+    library(distr)
+    
+    lowertail <- input$rangex[1]
+    uppertail <- input$rangex[2]
+    total_random_variables <- input$nrands
+    
+    dataX <-runif(total_random_variables, min=lowertail, max= uppertail )
+    
+    
+    dataX.df <- as.data.frame( dataX )
+    
+    
+    output$summary<-renderPrint({
+      dataX.df
+    })
+    
+    value = ifelse(input$greater,"g","l")
+    result <- t.test(dataX.df,alternative=value,mu=0.3)
+    
+    output$corr<-renderPrint({
+      result
+    })
+    
+    
+    #__________________________________________________________________________________
+    
+    
+    lowertail <- input$rangey[1]
+    uppertail <- input$rangey[2]
+    total_random_variables <- input$nrands
+    
+    dataY <-runif(total_random_variables, min=lowertail, max= uppertail)
+    
+    
+    dataY.df <- as.data.frame( dataY )
+    
+    output$tdataone<-renderPrint({
+      dataX.df
+    })
+    
+    output$tdatatwo<-renderPrint({
+      dataY.df
+    })
+    
+    value = ifelse(input$greater,"g","l")
+    result <- t.test(dataX.df, dataY.df, alternative=value,mu=0.3)
+    
+    output$trestwo<-renderPrint({
+      result
+    })
+    
+    #newdata <- input$files
+    #confirm load
+    output$caption<-renderText({
+      
+      if(is.null(input$files)) { "Load Data" }  else { "Data loaded" }
+      
+      
+    })
+  })
+  
+  
+  ###>>> Begin >>> Makarand Tawde (10527342) >>>
+  
+  RMSE <- 0
+  values = reactiveValues()
+  
+  #columns : independent columns
+  #columns2 : dependent column
+  
+  observe({
+    updateSelectInput(session,"columns",choices=colnames(myData()))
+    updateSelectInput(session,"columns2",choices=colnames(myData()))
+  })
+  
+  output$plot <- renderPlot({
+    if(input$Distribution=='Continuous')
+    {
+      
+      df <-na.omit(myData())
+      tarinddata <- cbind(df[,input$columns2],df[,input$columns])
+      colnames(tarinddata) = c(input$columns2,input$columns)
+      colnames(tarinddata)[1] <- "Y"
+      set.seed(199) 
+      
+      #fit.glm <- glm(y ~.,dataset, family="binomial") # ~. shows that we include all ind. variables 
+      
+      #summary(fit.glm)  
+      
+      n = nrow(tarinddata)  
+      
+      indexes = sample(n,n*(80/100))  
+      
+      trainset = data.frame(tarinddata[indexes,] ) 
+      
+      testset = data.frame(tarinddata[-indexes,] )
+      
+      # Fit the full model  
+      
+      actual=testset$Y
+      pred_test <- data.frame(testset)
+      
+      full.model <- glm(Y ~.,data=trainset, family='gaussian')
+      
+      #another.model <- glm(Y ~.,data=anotherdata, family='gaussian')
+      
+      
+      values$summ <- data.frame("significant among selected"=summary(full.model)$coeff[-1,4] < 0.05)
+      
+      
+      values$full <- full.model
+      
+      pred_full <- predict(full.model,testset[,input$columns])
+      
+      rmse_full = sqrt(sum((pred_full -actual)^2)/nrow(testset))
+      
+      
+      
+      
+      reduced.model =stepAIC(full.model) 
+      values$full = full.model
+      values$reduced <- reduced.model
+      pred_red = predict(reduced.model,testset[,input$columns])
+      
+      rmse_red = sqrt(sum((pred_red -actual)^2)/nrow(testset))
+      
+      values$rmse <- data.frame('Full model RMSE'=rmse_full,'Reduced model RMSE'=rmse_red)
+      values$Predictions <- data.frame('Full'=pred_full,'Reduced'=pred_red,'actual'=actual)
+      
+      par(mfrow=c(1,2))
+      plot(actual,type='o',col='black',xlab = 'observations',ylab=input$columns2,main='FULL')
+      
+      lines(pred_full,type='o',col='orange')
+      
+      legend(
+        "topleft",
+        lty=c(1,1),
+        col=c("black","orange"),
+        legend=c("Real","Predicted")
+      )
+      
+      plot(actual,type='o',col='black',xlab = 'observations',ylab=input$columns2,main='Reduced')
+      
+      lines(pred_red,type='o',col='orange')
+      
+      legend(
+        "topleft",
+        lty=c(1,1),
+        col=c("black","orange"),
+        legend=c("Real","Predicted")
+      )
+      
+    }
+    
+    else
+    {
+      df <-na.omit(myData())
+      tarinddata <- cbind(df[,input$columns2],df[,input$columns])
+      colnames(tarinddata) = c(input$columns2,input$columns)
+      colnames(tarinddata)[1] <- "Y"
+      mc = 1000
+      acc=0
+      
+      for(i in 1:mc){
+        
+        n = nrow(tarinddata)  
+        
+        indexes = sample(n,n*(80/100))  
+        
+        trainset = data.frame(tarinddata[indexes,] ) 
+        
+        testset = data.frame(tarinddata[-indexes,] )
+        
+        # Fit the full model  
+        
+        
+        pred_test <- data.frame(testset)
+        
+        model1<-glm(Y ~.,data=trainset, family='binomial')
+        
+        values$summ <- data.frame("significant among selected"=summary(model1)$coeff[-1,4] < 0.05)
+        
+        predy=predict(model1,testset)
+        pred_hat=ifelse(predy>=0.5,1,0)
+        actual=testset$Y
+        #Confusion Matrix
+        confusion_matrix=table(pred_hat,actual)
+        confusion_matrix
+        #Accuracy
+        acc <- acc +sum(confusion_matrix[row(confusion_matrix)==col(confusion_matrix)])/sum(confusion_matrix)
+        #acc <- acc +sum([row(df)==col(df)])/sum(df)
+        
+      }
+      accuracy2 = acc/mc
+      values$binom <- data.frame('Accuracy'=accuracy2)
+      values$binompredictions <- data.frame('Prediction'=pred_hat,'actual'=actual)
+      
+      par(mfrow=c(1,2))
+      plot(actual,type='o',col='black',xlab = 'observations',ylab=input$columns2)
+      
+      lines(pred_hat,type='o',col='orange')
+      
+      legend(
+        "topleft",
+        lty=c(1,1),
+        col=c("black","orange"),
+        legend=c("Real","Predicted")
+      )  
+    }
+    
+  })
+  
+  #Function for displaying selected data
+  
+  output$selData <- DT::renderDataTable({
+    df <- myData()
+    tarinddata <- cbind(df[,input$columns2],df[,input$columns])
+    colnames(tarinddata) = c(input$columns2,input$columns)
+    
+    DT::datatable(tarinddata,options = list(lengthChange = TRUE))
+  })
+  
+  
+  #Function for displaying the RMSE value 
+  
+  output$Measures <- DT::renderDataTable({
+    if(input$Distribution=='Continuous')
+    {
+      DT::datatable(values$rmse,options=list(lengthChange=TRUE))
+      
+    }
+    else
+    {
+      DT::datatable(values$binom,options=list(lengthChange=TRUE))  
+      
+    }
+    
+  })
+  
+  
+  #Function for displaying Predictions 
+  
+  output$Predictions <- DT::renderDataTable({
+    if(input$Distribution=='Continuous')
+    {
+      
+      DT::datatable(values$Predictions,options=list(lengthChange=TRUE))
+      
+    }
+    else
+    {
+      
+      DT::datatable(values$binompredictions,options=list(lengthChange=TRUE))
+      
+    }
+  })
+  
+  
+  #Function for displaying Significant selected columns
+  
+  output$Summary <- DT::renderDataTable({
+    DT::datatable(values$summ,options=list(lengthChange=TRUE))
+    
   })
 
 }
--- ui.R
+++ ui.R
@@ -1,3 +1,17 @@
+### Application name : CA1 B9DA101 - Statistics for Data Analytics 
+
+### Course : MSc (Data Analytics) - Sep 2019 - Group <C>  
+
+### Developed by :
+###   Abhishek Bhayye (10527126)
+###   Ashwini Kadam (10527552)
+###   Himaja Mineni (10529371)
+###   Makarand Tawde (10527342) 
+
+### College : Dublin Business School  
+
+###>>> Begin >>> Abhishek Bhayye (10527126) App Structure with overview>>> 
+
 library(shiny)
 library(shinydashboard)
 library(markdown)
@@ -62,42 +76,204 @@ dashboardPage(
             
             
           )
+        ),
+        
+        fluidRow(
+          column(
+            dataTableOutput("value"), width = 12)
         )
       ),
       
       # Second tab content
       tabItem(tabName = "overview",
-        h5("You will see all the information regarding the dataset here."),
+        h3("You will see all the information regarding the dataset here."),
         br(),
         fluidRow(
           
           valueBoxOutput("progressBox"),
 
           infoBoxOutput("progressBox2")
-        ),
-        fluidRow(
-          column(
-            dataTableOutput("value"), width = 12)
         )
-        
       ),
+      
+      ###>>> Begin >>> Ashwini Kadam(Student#10527552) >>> 
       
       # Third tab content
       tabItem(tabName = "model",
-        h3("Discrete Probability Models"),
-        source("model.R")
+        h2("Discrete Probability Models"),
+        fluidPage(
+          h3(""),
+          sidebarPanel( 
+            selectInput("dismodel", "Select Model", 
+                        choices = c(
+                          "Bernoulli" = "bernoulli",
+                          "Binomial" = "binomial", 
+                          "Poisson" = "poisson", 
+                          "Geometric" = "geometric",
+                          "Hypergeometric" = "hypergeometric"), 
+                        selected = "bernoulli" 
+            ), 
+            
+            conditionalPanel(
+              condition = "input.dismodel == 'bernoulli'",
+              # Slider input for the probability of successful trail
+              sliderInput("p", "Probability of successful trail(p)", min=0, max=1, step = 0.01, value = 0.5)
+            ),
+            
+            conditionalPanel( 
+              condition = "input.dismodel == 'binomial'", 
+              numericInput("n", "parameter n in Binomial" , value = 10), 
+              numericInput("p", "parameter p in Binomial" , value = 0.5) 
+            ), 
+            
+            conditionalPanel(     
+              condition = "input.dismodel == 'poisson'", 
+              numericInput("lam", "parameter lambda in Poisson" , value = 1) 
+            ), 
+            
+            conditionalPanel(     
+              condition = "input.dismodel == 'geometric'", 
+              numericInput("p", "parameter p in Geometric" , value = 0.5) 
+            ), 
+            
+            numericInput("max", "upper limit for x" , value = 5),  
+            sliderInput("s", "number of simulated data" ,min=1, max=1000, value = 10),  
+            
+            conditionalPanel( 
+              condition = "input.dismodel == 'binomial'", 
+              numericInput("j1", "j for Bin" , value = 1) 
+            ), 
+            
+            conditionalPanel( 
+              condition = "input.dismodel == 'poisson'", 
+              numericInput("j2", "j for Poisson" , value = 1) 
+            ), 
+            
+            conditionalPanel( 
+              condition = "input.dismodel == 'geometric'", 
+              numericInput("j3", "j for geometric" , value = 1) 
+            ),
+            
+            conditionalPanel(
+              condition = "input.dismodel == 'hypergeometric'",
+              numericInput("m", "M" , value = 10),
+              numericInput("n", "N" , value = 20),
+              numericInput("k", "K" , value = 5)
+            )
+          ),  
+          
+          mainPanel(  
+            plotOutput("histogram") 
+            # tableOutput('tab')  
+          )
+        )
       ),
+      
+      ###>>> Begin >>> Himaja Mineni (10529371) >>>
       
       # Fourth tab content
       tabItem(tabName = "hypothesis",
-        h3("Overview of Hypothesis using T-test"),
-        source("hypothesis.R")
+        h2("Overview of Hypothesis using T-test"),
+        pageWithSidebar(
+          # title
+          headerPanel(""),
+          
+          #input
+          sidebarPanel
+          (
+            sliderInput("rangex", "Select Parameter  X:", 
+                        min = 1, max = 100,  value = c(1,50), step= 1),
+            sliderInput("rangey", "Select {Range Parameter for Y:", 
+                        min = 1, max = 100,  value = c(1,50), step= 1),
+            numericInput("nrands","NO Random Numbers to be Generated?", value = 20,min = 5,step = 1),
+            h3("Alternative Attribute in t.test Function"),
+            checkboxInput("greater","Greater", value = TRUE),
+            checkboxInput("less","Less"),
+            actionButton("do", "Test_of_mean")
+          ), 
+          mainPanel( width = 8,
+            fluidRow(
+              h3(""),
+              tabsetPanel(
+                tabPanel("One Variable : T-Test",
+                  fluidRow(
+                    column(12,
+                           
+                      box(
+                        width = 3,
+                        h5("Random Numbers for X"),
+                        verbatimTextOutput("summary")  
+                      ),
+                      
+                      box(
+                        width = 9,
+                        h5("T-Test for mean"),
+                        verbatimTextOutput("corr") 
+                      )
+                    )
+                  )
+                ),
+                tabPanel("Two variable T-Test",
+                  fluidRow(
+                    column(12,
+                      
+                      box(
+                        width = 3,
+                        h5("Random Numbers for X"),
+                        verbatimTextOutput("tdataone")
+                      ),
+                      
+                      box(
+                        width = 3,
+                        h5("Random Numbers for Y"),
+                        verbatimTextOutput("tdatatwo")  
+                      ),
+                      
+                      box(
+                        width = 6,
+                        h5("T-Test for (x,y) "),
+                        verbatimTextOutput("trestwo")  
+                      )
+                    
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
       ),
+      
+      ###>>> Begin >>> Makarand Tawde (10527342) >>>
       
       # Fifth tab content
       tabItem(tabName = "glm",
-        h3("Prediction using GLM"),
-        source("glm.R")
+        h2("Prediction using GLM"),
+        pageWithSidebar(
+          headerPanel(""),
+          
+          sidebarPanel(
+            #Selector for file upload
+            fileInput('datafile', 'Choose CSV file',
+                      accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+            #These column selectors are dynamically created when the file is loaded
+            #uiOutput("Independent"),
+            #uiOutput("Dependent"),
+            selectInput("columns",label="Please select independent variables",multiple=TRUE,choices =""),
+            selectInput("columns2",label="Please select dependent variable",choices =""),
+            selectInput("Distribution","Please select distribution type",
+                        choices = c("Discrete","Continuous"))
+            
+          ),
+          mainPanel(
+            tabsetPanel(type = 'tab',
+              tabPanel("Selected cols",DT::dataTableOutput("selData")),
+              tabPanel("Actual vs Predicted",plotOutput("plot")),
+              tabPanel("Summary",DT::dataTableOutput("Summary")),
+              tabPanel("Measures",DT::dataTableOutput("Measures")),
+              tabPanel("Prediction",DT::dataTableOutput("Predictions")))
+          )
+        )
       )
     )
   )
