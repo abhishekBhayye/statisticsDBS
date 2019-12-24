112191a9e8087c326ef497b112bed4d049dd7aea
tawdemakarand777@gmail.com <tawdemakarand777@gmail.com>
2019-12-24 21:19:29 +0000

    Adding comments and final cleanup

--- glm.R
+++ glm.R
@@ -1,3 +1,13 @@
+### Application name : CA1 B9DA101 - Statistics for Data Analytics 
+
+### Course : MSc (Data Analytics) - Sep 2019 - Group <C>  
+
+### Developed by : Makarand Tawde (10527342) 
+
+### College : Dublin Business School  
+
+###>>> Begin >>> Makarand Tawde (10527342) >>> 
+
 library(shiny)
 library(pdfetch)
 library(MASS)
@@ -8,6 +18,7 @@ server <- function(input, output,session) {
   values = reactiveValues()
   #This function is repsonsible for loading in the selected file
   filedata <- reactive({
+    
     #validating independent variables if not selected
     validate(
       need(input$datafile != "", "Please select a data set")
@@ -43,11 +54,7 @@ server <- function(input, output,session) {
       colnames(tarinddata)[1] <- "Y"
       set.seed(199) 
       
-      #fit.glm <- glm(y ~.,dataset, family="binomial") # ~. shows that we include all ind. variables 
-      
-      #summary(fit.glm)  
-      
-      n = nrow(tarinddata)  
+      n = nrow(tarinddata) 
       
       indexes = sample(n,n*(80/100))  
       
@@ -58,14 +65,13 @@ server <- function(input, output,session) {
       # Fit the full model  
       
       actual=testset$Y
+      
       pred_test <- data.frame(testset)
       
       full.model <- glm(Y ~.,data=trainset, family='gaussian')
       
-      #another.model <- glm(Y ~.,data=anotherdata, family='gaussian')
       
-      
-      values$summ <- data.frame("significant among selected"=summary(full.model)$coeff[-1,4] < 0.05)
+      values$summ <- data.frame("significant columns among selected"=summary(full.model)$coeff[-1,4] < 0.05)
       
       
       values$full <- full.model
@@ -76,7 +82,7 @@ server <- function(input, output,session) {
       
       
       
-      
+      #Calculations for reduced model
       reduced.model =stepAIC(full.model) 
       values$full = full.model
       values$reduced <- reduced.model
@@ -85,9 +91,12 @@ server <- function(input, output,session) {
       rmse_red = sqrt(sum((pred_red -actual)^2)/nrow(testset))
       
       values$rmse <- data.frame('Full model RMSE'=rmse_full,'Reduced model RMSE'=rmse_red)
-      values$Predictions <- data.frame('Full'=pred_full,'Reduced'=pred_red,'actual'=actual)
+      values$Predictions <- data.frame('Full'=pred_full,'Reduced'=pred_red)
       
       par(mfrow=c(1,2))
+      
+      #plot for full model
+      
       plot(actual,type='o',col='black',xlab = 'observations',ylab=input$columns2,main='FULL')
       
       lines(pred_full,type='o',col='orange')
@@ -98,6 +107,8 @@ server <- function(input, output,session) {
         col=c("black","orange"),
         legend=c("Real","Predicted")
       )
+      
+      #Plot for reduced model
       
       plot(actual,type='o',col='black',xlab = 'observations',ylab=input$columns2,main='Reduced')
       
@@ -114,6 +125,8 @@ server <- function(input, output,session) {
     
     else
     {
+      #Calculations and plot for Discrete target column
+      #Note: Only full model approach used
       df <-na.omit(filedata())
       tarinddata <- cbind(df[,input$columns2],df[,input$columns])
       colnames(tarinddata) = c(input$columns2,input$columns)
@@ -138,7 +151,7 @@ server <- function(input, output,session) {
         
         model1<-glm(Y ~.,data=trainset, family='binomial')
         
-        values$summ <- data.frame("significant among selected"=summary(model1)$coeff[-1,4] < 0.05)
+        values$summ <- data.frame("significant columns among selected"=summary(model1)$coeff[-1,4] < 0.05)
         
         predy=predict(model1,testset)
         pred_hat=ifelse(predy>=0.5,1,0)
@@ -148,12 +161,11 @@ server <- function(input, output,session) {
         confusion_matrix
         #Accuracy
         acc <- acc +sum(confusion_matrix[row(confusion_matrix)==col(confusion_matrix)])/sum(confusion_matrix)
-        #acc <- acc +sum([row(df)==col(df)])/sum(df)
         
       }
       accuracy2 = acc/mc
       values$binom <- data.frame('Accuracy'=accuracy2)
-      values$binompredictions <- data.frame('Prediction'=pred_hat,'actual'=actual)
+      values$binompredictions <- data.frame('Prediction'=pred_hat)
       
       par(mfrow=c(1,2))
       plot(actual,type='o',col='black',xlab = 'observations',ylab=input$columns2)
@@ -176,7 +188,7 @@ server <- function(input, output,session) {
   
   
   
-  #Function for displaying selected data
+  #Function for displaying selected column data
   
   output$selData <- DT::renderDataTable({
     df <- filedata()
@@ -187,7 +199,9 @@ server <- function(input, output,session) {
   })
   
   
-  #Function for displaying the RMSE value 
+  #Function for displaying the measure of Performances
+  #For continuous: RMSE
+  #For Discrete: Accuracy
   
   output$Measures <- DT::renderDataTable({
     if(input$Distribution=='Continuous')
@@ -233,19 +247,32 @@ server <- function(input, output,session) {
   
 }
 
+###<<< End   <<< Makarand Tawde (10527342) <<<  
 
 ##############################################################################################
+### Application name : CA1 B9DA101 - Statistics for Data Analytics 
+
+### Course : MSc (Data Analytics) - Sep 2019 - Group <C>  
+
+### Developed by : Makarand Tawde (10527342) 
+
+### College : Dublin Business School  
+
+###>>> Begin >>> Makarand Tawde (10527342) >>> 
+
 
 ui <- pageWithSidebar(
   headerPanel("Prediction using GLM"),
   
   sidebarPanel(
+    
     #Selector for file upload
     fileInput('datafile', 'Choose CSV file',
               accept=c('text/csv', 'text/comma-separated-values,text/plain')),
     #These column selectors are dynamically created when the file is loaded
     #uiOutput("Independent"),
     #uiOutput("Dependent"),
+    helpText("In case of Continuous target var choose atleast 2 independent variables"),
     selectInput("columns",label="Please select independent variables",multiple=TRUE,choices =""),
     selectInput("columns2",label="Please select dependent variable",choices =""),
     selectInput("Distribution","Please select distribution type",
@@ -261,5 +288,5 @@ ui <- pageWithSidebar(
                 tabPanel("Prediction",DT::dataTableOutput("Predictions")))
   )
 )
-
+###<<< End   <<< Makarand Tawde (10527342) <<< 
 shinyApp(ui, server)
